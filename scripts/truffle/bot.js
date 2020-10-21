3bab7e4ef1329744a9b214b51643d7862f19d223
Jitendra Chittoda <jcchittoda@gmail.com>
2020-10-21 20:59:54 +0530

    topup working

--- scripts/truffle/bot.js
+++ scripts/truffle/bot.js
@@ -1,9 +1,4 @@
 const BN = require("bn.js");
-const { time } = require("@openzeppelin/test-helpers");
-
-require("@openzeppelin/test-helpers/configure")({
-    provider: "http://localhost:2000",
-});
 
 const abiJSON = require("../../lib/dss-cdp-manager/out/dapp.sol.json");
 const mcdJSON = require("../../config/mcd_testchain.json");
@@ -51,164 +46,208 @@ let MEMBER_4 = bpJSON.MEMBER_4;
 
 const ILK_ETH = web3.utils.padRight(web3.utils.asciiToHex("ETH-A"), 64);
 
+const topped = new Map();
+const bitten = new Map();
+
 module.exports = async function (callback) {
-    bCdpManager = await BCdpManager.at(bpJSON.B_CDP_MANAGER);
-    pool = await Pool.at(bpJSON.POOL);
-    dai = await Dai.at(mcdJSON.MCD_DAI);
-    daiJoin = await DaiJoin.at(mcdJSON.MCD_JOIN_DAI);
-    dssCdpManager = await DssCdpManager.at(mcdJSON.CDP_MANAGER);
-    gemJoin = await GemJoin.at(mcdJSON.MCD_JOIN_ETH_A);
-    vat = await Vat.at(mcdJSON.MCD_VAT);
-    osm = await OSM.at(mcdJSON.PIP_ETH);
-    spot = await Spotter.at(mcdJSON.MCD_SPOT);
+  bCdpManager = await BCdpManager.at(bpJSON.B_CDP_MANAGER);
+  pool = await Pool.at(bpJSON.POOL);
+  dai = await Dai.at(mcdJSON.MCD_DAI);
+  daiJoin = await DaiJoin.at(mcdJSON.MCD_JOIN_DAI);
+  dssCdpManager = await DssCdpManager.at(mcdJSON.CDP_MANAGER);
+  gemJoin = await GemJoin.at(mcdJSON.MCD_JOIN_ETH_A);
+  vat = await Vat.at(mcdJSON.MCD_VAT);
+  osm = await OSM.at(mcdJSON.PIP_ETH);
+  spot = await Spotter.at(mcdJSON.MCD_SPOT);
+  const gem = await gemJoin.gem();
+  weth = await WETH.at(gem);
 
-    try {
-        await loadWETH();
+  try {
+    // initialize
+    await init();
 
-        // await setupTestchain();
-
-        /*
-    const daiInRad = HUNDRED_DAI.mul(RAY);
-    await memberDeposit(daiInRad, { from: MEMBER_1 });
-    await memberDeposit(daiInRad, { from: MEMBER_2 });
-    await memberDeposit(daiInRad, { from: MEMBER_3 });
-    await memberDeposit(daiInRad, { from: MEMBER_4 });
-    
-    await memberWithdraw(daiInRad, { from: MEMBER_1 });
-    await memberWithdraw(daiInRad, { from: MEMBER_2 });
-    await memberWithdraw(daiInRad, { from: MEMBER_3 });
-    await memberWithdraw(daiInRad, { from: MEMBER_4 });
-    */
-
-        // await getTopAmount(1);
-
-        await setPrice(new BN(150).mul(ONE_ETH));
-        // await getTopAmount(1);
-    } catch (err) {
+    web3.eth.subscribe("newBlockHeaders", async (error, event) => {
+      try {
+        if (!error) {
+          console.log("Block: " + event.number);
+          await processCdps();
+          await processBite();
+        } else {
+          console.log(error);
+        }
+      } catch (err) {
         console.log(err);
-    }
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  while (true) {
+    await sleep(100);
+  }
 };
 
-async function getTopAmount(cdp) {
-    // cdp = openCdp(1 ether, 110 ether);
-    const amt = await pool.topAmount(cdp);
-    console.log(amt);
-    // const bud = await pool.osm(ILK_ETH);
-    // console.log("bud " + bud);
-    // const budConnector = await BudConnector.at(bud);
-    // const osm = await budConnector.osm();
-    // console.log(osm);
-}
-
-async function setPrice(price) {
-    console.log("Current price: " + (await getCurrentPrice()).toString());
-
-    await osm.kiss(mcdJSON.DEPLOYER);
-    const val = await DSValue.at(mcdJSON.VAL_ETH);
-    const bytes32 = uintToBytes32(price);
-    await val.poke(bytes32);
-
-    const pass = await osm.pass({ from: mcdJSON.DEPLOYER });
-    if (pass) {
-        // to poke empty next, only for the first time
-        await osm.poke();
+async function processCdps() {
+  let cdp = 1;
+  while (true) {
+    const exist = await isCdpExists(cdp);
+    if (exist) {
+      // console.log("Checking cdp: " + cdp);
+      await processTopup(cdp);
+    } else {
+      // console.log("Cdp not exists: " + cdp);
+      break;
     }
-
-    const hop = await osm.hop();
-    await time.increase(hop);
-
-    await osm.poke();
-    await spot.poke(ILK_ETH);
-
-    console.log("New price: " + (await getCurrentPrice()).toString());
+    cdp++;
+  }
 }
 
-async function getCurrentPrice() {
-    const read = await osm.read({ from: mcdJSON.DEPLOYER });
-    return bytes32ToBN(read);
+async function processBite() {
+  let cdp = 1;
+  while (true) {
+    const toppedUp = await topped.get(cdp);
+    const isBitten = bitten.get(cdp);
+    if(toppedUp && !isBitten) {
+      const avail = await pool.availBite.call(cdp, MEMBER_1, {from: MEMBER_1});
+      console.log("cdp: " + cdp + " avail: "+ avail);
+      const dMemberInk = await pool.bite.call(cdp, avail, 0, {from: MEMBER_1});
+      console.log("memberInk: " +dMemberInk);
+
+      try{
+        // try block to catch failed tx
+        await pool.bite(cdp, avail, 0, {from: MEMBER_1});
+      }catch(err) {
+        console.log(err);
+      }
+      bitten.set(cdp, true);
+    } else {
+      break;
+    }
+    cdp++;
+  }
 }
 
-function uintToBytes32(val) {
-    return web3.utils.padLeft(web3.utils.numberToHex(val), 64);
+async function processTopup(cdp) {
+  const allowed = await isTopupAllowed(cdp);
+  if (allowed) {
+    if (topped.get(cdp)) {
+      return;
+    }
+    console.log("topup allowed: " + cdp);
+    await memberTopup(cdp, { from: MEMBER_1 });
+    topped.set(cdp, true);
+  } else {
+    
+    //console.log("topup not allowed: " + cdp);
+  }
 }
 
-function bytes32ToBN(val) {
-    return new BN(web3.utils.hexToNumberString(val));
+async function isCdpExists(cdp) {
+  const urn = await bCdpManager.urns(cdp);
+  return urn != 0;
 }
 
-async function loadWETH() {
-    const gem = await gemJoin.gem();
-    weth = await WETH.at(gem);
+async function isTopupAllowed(cdp) {
+  const info = await pool.topupInfo(cdp);
+  const dart = info[0];
+  const should = info[3];
+  return should && dart != 0;
 }
 
-async function setupTestchain() {
-    await mintDaiForMember(1, 100, { from: MEMBER_1 }); // 1 eth, 100 DAI
-    await mintDaiForMember(1, 100, { from: MEMBER_2 });
-    await mintDaiForMember(1, 100, { from: MEMBER_3 });
-    await mintDaiForMember(1, 100, { from: MEMBER_4 });
+async function init() {
+  await mintDaiForMember(20, 1000, { from: MEMBER_1 });
+  await memberDeposit(HUNDRED_DAI.mul(RAY), { from: MEMBER_1 });
 }
 
-async function mintDaiForMember(amtInEth, amtInDai, opt) {
-    const _from = opt.from;
-    ink = new BN(amtInEth).mul(new BN(ONE_ETH));
-    art = new BN(amtInDai).mul(new BN(ONE_ETH));
-
-    // DssCdpManager.open();
-    const cdp = await dssCdpManager.open.call(ILK_ETH, _from, {
-        from: _from,
-    });
-    await dssCdpManager.open(ILK_ETH, _from, { from: _from });
-
-    // WETH.deposit()
-    await web3.eth.sendTransaction({
-        from: _from,
-        to: weth.address,
-        value: ink,
-    });
-
-    // WETH.approve()
-    await weth.approve(gemJoin.address, ink, { from: _from });
-
-    // ethJoin.join()
-    const urn = await dssCdpManager.urns(cdp);
-    await gemJoin.join(urn, ink, { from: _from });
-
-    // DssCdpManager.frob()
-    await dssCdpManager.frob(cdp, ink, art, { from: _from });
-
-    // DssCdpManager.move();
-    await dssCdpManager.move(cdp, _from, art.mul(RAY), {
-        from: _from,
-    });
-
-    console.log("Minted: " + amtInDai + " DAI for user:" + _from);
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function memberDeposit(radVal, opt) {
-    const _from = opt.from;
-    await vat.hope(pool.address, { from: _from });
-
-    await pool.deposit(radVal, { from: _from });
-
-    console.log("Member:" + _from + " deposited: " + radToDAI(radVal) + " DAI");
+  const _from = opt.from;
+  await vat.hope(pool.address, { from: _from });
+  await pool.deposit(radVal, { from: _from });
+  console.log("Member:" + _from + " deposited: " + radToDAI(radVal) + " DAI");
 }
 
 function radToDAI(radVal) {
-    return radVal.div(RAY).div(ONE_ETH).toString();
+  return radVal.div(RAY).div(ONE_ETH).toString();
 }
 
 function wadToDAI(wad) {
-    return radVal.div(ONE_ETH).toString();
+  return radVal.div(ONE_ETH).toString();
 }
 
-async function memberTopup() {}
+async function memberTopup(cdp, opt) {
+  try {
+    const info = await pool.topupInfo(cdp);
+    console.log(info);
 
-async function memberUntop() {}
+    const _from = opt.from;
+    await pool.topup(cdp, { from: _from });
+    console.log("Member topped up: " + cdp);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-async function memberBite() {}
+async function memberUntop(cdp, opt) {
+  const _from = opt.from;
+  await pool.untop(cdp, { from: _from });
+  console.log("Member untop: " + cdp);
+}
+
+async function memberBite(cdp, dart, minInk, opt) {
+  const _from = opt.from;
+  await pool.bite(cdp, dart, minInk, { from: _from });
+  console.log("Member bite: " + cdp);
+}
 
 async function memberWithdraw(radVal, opt) {
-    const _from = opt.from;
-    await pool.withdraw(radVal, { from: _from });
-    console.log("Member:" + _from + " withdrew: " + radToDAI(radVal) + " DAI");
+  const _from = opt.from;
+  await pool.withdraw(radVal, { from: _from });
+  console.log("Member:" + _from + " withdrew: " + radToDAI(radVal) + " DAI");
+}
+
+async function mintDai(manager, amtInEth, amtInDai, isMove, opt) {
+  const _from = opt.from;
+  const ink = new BN(amtInEth).mul(new BN(ONE_ETH));
+  const art = new BN(amtInDai).mul(new BN(ONE_ETH));
+
+  // manager.open();
+  const cdp = await manager.open.call(ILK_ETH, _from, {
+    from: _from,
+  });
+  await manager.open(ILK_ETH, _from, { from: _from });
+
+  // WETH.deposit()
+  await web3.eth.sendTransaction({
+    from: _from,
+    to: weth.address,
+    value: ink,
+  });
+
+  // WETH.approve()
+  await weth.approve(gemJoin.address, ink, { from: _from });
+
+  // ethJoin.join()
+  const urn = await manager.urns(cdp);
+  await gemJoin.join(urn, ink, { from: _from });
+
+  // manager.frob()
+  await manager.frob(cdp, ink, art, { from: _from });
+
+  if (isMove) {
+    // manager.move();
+    await manager.move(cdp, _from, art.mul(RAY), {
+      from: _from,
+    });
+  }
+}
+
+async function mintDaiForMember(amtInEth, amtInDai, opt) {
+  const _from = opt.from;
+  await mintDai(dssCdpManager, amtInEth, amtInDai, true, opt);
+  console.log("Minted: " + amtInDai + " DAI for MEMBER:" + opt.from);
 }
---
+++ test/test.js
@@ -0,0 +1,206 @@
+const BN = require("bn.js");
+const { time } = require("@openzeppelin/test-helpers");
+
+require("@openzeppelin/test-helpers/configure")({
+  provider: "http://localhost:2000",
+});
+
+const abiJSON = require("../lib/dss-cdp-manager/out/dapp.sol.json");
+const mcdJSON = require("../config/mcd_testchain.json");
+const bpJSON = require("../config/bprotocol_testchain.json");
+
+const RAY = new BN(10).pow(new BN(27));
+const ONE_ETH = new BN(10).pow(new BN(18));
+const ILK_ETH = web3.utils.padRight(web3.utils.asciiToHex("ETH-A"), 64);
+
+const BCdpManager = artifacts.require("BCdpManager");
+const DssCdpManager = artifacts.require("DssCdpManager");
+const WETH = artifacts.require("WETH");
+const GemJoin = artifacts.require("GemJoin");
+const OSM = artifacts.require("OSM");
+const DSValue = artifacts.require("DSValue");
+const MockDaiToUsdPriceFeed = artifacts.require("MockDaiToUsdPriceFeed");
+
+let bCdpManager;
+let dssCdpManager;
+let weth;
+let gemJoin;
+let osm;
+let dai2usd;
+
+let USER_1 = "0xda1495ebd7573d8e7f860862baa3abecebfa02e0";
+
+let MEMBER_1 = bpJSON.MEMBER_1;
+let MEMBER_2 = bpJSON.MEMBER_2;
+let MEMBER_3 = bpJSON.MEMBER_3;
+let MEMBER_4 = bpJSON.MEMBER_4;
+
+contract("Testchain", (accounts) => {
+  before(async () => {
+    bCdpManager = await BCdpManager.at(bpJSON.B_CDP_MANAGER);
+    dssCdpManager = await DssCdpManager.at(mcdJSON.CDP_MANAGER);
+    gemJoin = await GemJoin.at(mcdJSON.MCD_JOIN_ETH_A);
+    const gem = await gemJoin.gem();
+    weth = await WETH.at(gem);
+    osm = await OSM.at(mcdJSON.PIP_ETH);
+    dai2usd = await MockDaiToUsdPriceFeed.at(bpJSON.DAI2USD);
+
+    // await init();
+  });
+
+  // it("increaseTime", async () => {
+  //   const nextTime = Number(await osm.zzz()) + parseInt(Number(await osm.hop()) / 2) + 1;
+  //   await time.increase(nextTime);
+  //   console.log("Current price: " + (await getCurrentPrice()).toString());
+  //   console.log("Next price: " + (await getNextPrice()).toString());
+  //   console.log((await osm.zzz()).toString());
+  //   console.log((await osm.hop()).toString());
+  //   const blocktime = (await web3.eth.getBlock("latest")).timestamp;
+  //   console.log(blocktime);
+  // });
+
+  // 1.
+  it("mint DAI for User", async () => {
+    await mintDaiForUser(2, 199, { from: USER_1 });
+  });
+
+  // 2.
+  it("setNextPrice", async () => {
+    const nextTime = Number(await osm.zzz()) + parseInt(Number(await osm.hop()) / 2) + 1;
+    await time.increase(nextTime);
+    await setNextPrice(new BN(145).mul(ONE_ETH));
+    await dai2usd.setPrice(new BN(145).mul(ONE_ETH));
+    console.log("Current price: " + (await getCurrentPrice()).toString());
+    await increaseHalfHour();
+  });
+
+  // 3.
+  // it("poke", async () => {
+    // await increaseHalfHour();
+    // await osm.poke();
+    // console.log("Current price: " + (await getCurrentPrice()).toString());
+  // });
+});
+
+async function init() {
+  const nextTime = Number(await osm.zzz()) + parseInt(Number(await osm.hop()) / 2) + 1;
+  await time.increase(nextTime);
+
+  await osm.kiss(mcdJSON.DEPLOYER);
+  const val = await DSValue.at(mcdJSON.VAL_ETH);
+  const bytes32 = uintToBytes32(new BN(150).mul(ONE_ETH));
+  await val.poke(bytes32);
+  await osm.poke();
+
+  await time.increase(3600);
+  await osm.poke();
+
+  console.log("Current price: " + (await getCurrentPrice()).toString());
+}
+
+async function increaseHalfHour() {
+  const hop = await osm.hop();
+  await time.increase(hop / 2 + 1);
+}
+
+async function setupTestchain() {
+  await mintDaiForMember(20, 1000, { from: MEMBER_1 });
+  // await mintDaiForMember(20, 1000, { from: MEMBER_2 });
+  // await mintDaiForMember(20, 1000, { from: MEMBER_3 });
+  // await mintDaiForMember(20, 1000, { from: MEMBER_4 });
+}
+
+async function mintDaiForUser(amtInEth, amtInDai, opt) {
+  await mintDai(bCdpManager, amtInEth, amtInDai, false, opt);
+  console.log("Minted: " + amtInDai + " DAI for USER:" + opt.from);
+}
+
+async function mintDaiForMember(amtInEth, amtInDai, opt) {
+  const _from = opt.from;
+  await mintDai(dssCdpManager, amtInEth, amtInDai, true, opt);
+  console.log("Minted: " + amtInDai + " DAI for MEMBER:" + opt.from);
+}
+
+async function mintDai(manager, amtInEth, amtInDai, isMove, opt) {
+  const _from = opt.from;
+  const ink = new BN(amtInEth).mul(new BN(ONE_ETH));
+  const art = new BN(amtInDai).mul(new BN(ONE_ETH));
+
+try{
+  // manager.open();
+  const cdp = await manager.open.call(ILK_ETH, _from, {
+    from: _from,
+  });
+  await manager.open(ILK_ETH, _from, { from: _from });
+
+  // WETH.deposit()
+  await web3.eth.sendTransaction({
+    from: _from,
+    to: weth.address,
+    value: ink,
+  });
+
+  // WETH.approve()
+  await weth.approve(gemJoin.address, ink, { from: _from });
+
+  // ethJoin.join()
+  const urn = await manager.urns(cdp);
+  await gemJoin.join(urn, ink, { from: _from });
+
+  // manager.frob()
+  await manager.frob(cdp, ink, art, { from: _from });
+
+  if (isMove) {
+    // manager.move();
+    await manager.move(cdp, _from, art.mul(RAY), {
+      from: _from,
+    });
+  }
+}catch(err) {
+  console.log(err);
+}
+}
+
+async function poke() {
+  // const pass = await osm.pass({ from: mcdJSON.DEPLOYER });
+  // if (pass) {
+  //     // to poke empty next, only for the first time
+  //     await osm.poke();
+  // }
+
+  await osm.poke();
+  await spot.poke(ILK_ETH);
+
+  console.log("New price: " + (await getCurrentPrice()).toString());
+}
+
+async function setNextPrice(price) {
+  console.log("Current price: " + (await getCurrentPrice()).toString());
+
+  await osm.kiss(mcdJSON.DEPLOYER);
+  const val = await DSValue.at(mcdJSON.VAL_ETH);
+  const bytes32 = uintToBytes32(price);
+  await val.poke(bytes32);
+
+  const pass = await osm.pass({ from: mcdJSON.DEPLOYER });
+  if (pass) await osm.poke();
+  console.log("Next price: " + (await getNextPrice()).toString());
+}
+
+async function getCurrentPrice() {
+  const read = await osm.read({ from: mcdJSON.DEPLOYER });
+  return bytes32ToBN(read);
+}
+
+async function getNextPrice() {
+  const peep = await osm.peep({ from: mcdJSON.DEPLOYER });
+  return bytes32ToBN(peep[0]);
+}
+
+function uintToBytes32(val) {
+  return web3.utils.padLeft(web3.utils.numberToHex(val), 64);
+}
+
+function bytes32ToBN(val) {
+  return new BN(web3.utils.hexToNumberString(val));
+}
